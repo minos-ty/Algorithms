eb5177d4fc09dccd52b8d5321812efc2ccc2ddbc
minos <tyforbyte@gmail.com>
2020-07-18 22:49:14 +0800

    version 1.1

--- src/com/linearsearch/LinearSearch.java
+++ src/com/linearsearch/LinearSearch.java
@@ -1,7 +1,7 @@
 package com.linearsearch;
 
 public class LinearSearch {
-    //类构造方法私有化，防止用户创建类
+    //类的构造方法私有化，防止用户创建类对象，使其只能通过类名直接调用方法
     private LinearSearch(){
     }
 
@@ -19,4 +19,23 @@ public class LinearSearch {
         }
         return -1;
     }
+
+    /**
+     * 改进：使用泛型让算法支持查找所有类型数组
+     * 注意️：Java中泛型只能接收类对象
+     * @param data
+     * @param target
+     * @param <E>
+     * @return
+     */
+    public static <E> int search(E[] data, E target){
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(target)){//引用数据类型判等用equals方法
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
--- src/com/linearsearch/LinearSearchDemo.java
+++ src/com/linearsearch/LinearSearchDemo.java
@@ -2,7 +2,12 @@ package com.linearsearch;
 
 public class LinearSearchDemo {
     public static void main(String[] args) {
-        int[] data = {24, 18, 12, 9, 16, 100, 21, 1};
-        System.out.println(LinearSearch.search(data, 21));
+        int[] dataInt = {24, 18, 12, 9, 16, 100, 21, 1};
+        Character[] dataChar = {'a', 'b', 'd', 'e', 'c', 'f'};
+        String[] dataString = {"tangyang", "minos", "汤洋"};
+
+        System.out.println(LinearSearch.search(dataInt, 21));
+        System.out.println(LinearSearch.search(dataChar, 'c'));
+        System.out.println(LinearSearch.search(dataString,"汤洋"));
     }
 }
