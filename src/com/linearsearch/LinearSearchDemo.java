8348bddd13a41066cda0f832e762a876c7a3359e
minos <tyforbyte@gmail.com>
2020-07-19 06:05:27 +0800

    version 1.1

--- src/com/linearsearch/LinearSearchDemo.java
+++ src/com/linearsearch/LinearSearchDemo.java
@@ -4,10 +4,16 @@ public class LinearSearchDemo {
     public static void main(String[] args) {
         int[] dataInt = {24, 18, 12, 9, 16, 100, 21, 1};
         Character[] dataChar = {'a', 'b', 'd', 'e', 'c', 'f'};
-        String[] dataString = {"tangyang", "minos", "汤洋"};
+        String[] dataString = {"tangyang", "minos", "大圣"};
+        Student[] dataStu = {
+                new Student("Minos", 18),
+                new Student("ty", 10),
+                new Student("fw", 19)
+        };
 
         System.out.println(LinearSearch.search(dataInt, 21));
         System.out.println(LinearSearch.search(dataChar, 'c'));
-        System.out.println(LinearSearch.search(dataString,"汤洋"));
+        System.out.println(LinearSearch.search(dataString, "大圣"));
+        System.out.println(LinearSearch.search(dataStu, new Student("fw", 19)));
     }
 }
