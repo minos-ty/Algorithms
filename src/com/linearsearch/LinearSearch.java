e67977ddb2efdb7930b57a8ad28f0e192f88147e
minos <tyforbyte@gmail.com>
2020-07-18 21:23:21 +0800

    version 1.0

---
+++ src/com/linearsearch/LinearSearch.java
@@ -0,0 +1,19 @@
+package com.linearsearch;
+
+public class LinearSearch {
+
+    /**
+     * 线性查找，根据目标元素返回数组中对应的下标
+     * @param data 数组
+     * @param target 目标元素
+     * @return 返回目标元素对应的下标,若数组中找不到目标元素则返回-1
+     */
+    public static int search(int[] data, int target){
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/com/linearsearch/LinearSearchDemo.java
@@ -0,0 +1,8 @@
+package com.linearsearch;
+
+public class LinearSearchDemo {
+    public static void main(String[] args) {
+        int[] data = {24, 18, 12, 9, 16, 100, 21, 1};
+        System.out.println(LinearSearch.search(data, 21));
+    }
+}
