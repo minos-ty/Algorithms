1ea58c6fac3054bf99a6d7a74ce8f5dc85890e79
minos <tyforbyte@gmail.com>
2020-07-31 19:59:01 +0800

    version 1.1

--- src/com/linearsearch/Student.java
+++ src/com/linearsearch/Student.java
@@ -6,10 +6,6 @@ public class Student {
     private String name;
     private int age;
 
-    public Student(){
-
-    }
-
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
