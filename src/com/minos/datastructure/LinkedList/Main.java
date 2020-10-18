d5b1745d915e25667a9927b2fcc180b446c117cd
minos <tyforbyte@gmail.com>
2020-10-18 18:07:50 +0800

    v1.1

---
+++ src/com/minos/datastructure/LinkedList/Main.java
@@ -0,0 +1,27 @@
+package com.minos.datastructure.LinkedList;
+
+/**
+ * @Author: minos
+ * @Date: 2020/10/17 13:38
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i =0; i < 5; i++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(3, 666);
+        System.out.println(linkedList);
+
+        linkedList.remove(3);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
+}
---
+++ src/com/minos/datastructure/stack/LinkedListStack.java
@@ -0,0 +1,32 @@
+package com.minos.datastructure.stack;
+
+/**
+ * @Author: minos
+ * @Date: 2020/10/18 18:06
+ */
+public class LinkedListStack<E> implements Stack<E> {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peak() {
+        return null;
+    }
+}
