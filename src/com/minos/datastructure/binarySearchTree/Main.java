5bbf82a92401986f09121a4e2a247a2a5b6a2c2f
minos <tyforbyte@gmail.com>
2020-10-21 11:10:38 +0800

    v1.1

---
+++ src/com/minos/datastructure/binarySearchTree/Main.java
@@ -0,0 +1,11 @@
+package com.minos.datastructure.binarySearchTree;
+
+/**
+ * @Author: minos
+ * @Date: 2020/10/20 22:02
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/minos/datastructure/recursion/RecursionArray.java
+++ src/com/minos/datastructure/recursion/RecursionArray.java
@@ -1,5 +1,9 @@
 package com.minos.datastructure.recursion;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @Author: minos
  * @Date: 2020/10/20 13:20
@@ -14,9 +18,42 @@ public class RecursionArray {
         return arr[l] + recursionSum(arr, l + 1);
     }
 
+    public static List<Integer> removeElem(List<Integer> list, int i){
+
+        if (list == null){
+            return null;
+        }
+
+
+
+        return null;
+    }
+
+
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3};
         System.out.println(recursionSum(arr, 0));
+
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
