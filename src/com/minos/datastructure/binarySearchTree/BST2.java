1df286cce15bf823197e6526521a7d074c6e7eb9
minos <tyforbyte@gmail.com>
2020-10-22 22:20:32 +0800

    v1.1

--- src/com/minos/datastructure/binarySearchTree/BST2.java
+++ src/com/minos/datastructure/binarySearchTree/BST2.java
@@ -1,17 +1,22 @@
 package com.minos.datastructure.binarySearchTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * 改进的代码简化的实现
+ *
  * @Author: minos
  * @Date: 2020/10/21 21:20
  */
-public class BST2<E extends Comparable<E> > {
+public class BST2<E extends Comparable<E>> {
 
-    private class Node{
+    private class Node {
         public E e;
         public Node left, right;
 
-        public Node(E e){
+        public Node(E e) {
             this.e = e;
             left = null;
             right = null;
@@ -21,48 +26,50 @@ public class BST2<E extends Comparable<E> > {
     private Node root;
     private int size;
 
-    public BST2(){
+    public BST2() {
         root = null;
         size = 0;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public boolean isEmpty(){
-        return size==0;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     /**
      * 向二分搜索树种中添加新元素
+     *
      * @param e
      */
-    public void add(E e){
+    public void add(E e) {
         root = add(root, e);
     }
 
     /**
      * 向以node为根的二分搜索树中插入元素e，递归算法
+     *
      * @param node
      * @param e
      * @return 返回插入新节点后二分搜索树的根
      */
-    private Node add(Node node, E e){
+    private Node add(Node node, E e) {
 
         //插入动作，递归终止条件
-        if(node == null){
+        if (node == null) {
             size++;
             return new Node(e);
         }
 
         //如果待插入元素小于节点中的元素，直接向左孩子递归
-        if (e.compareTo(node.e) < 0){
+        if (e.compareTo(node.e) < 0) {
             //如果当前节点的左孩子为null,生成新节点后直接返回，node.left = 新生成的节点
             node.left = add(node.left, e);
 
-        //用else if 判断后相当于排除了第三种可能，既待插入元素值与当前节点值相等时什么也不做
-        } else if (e.compareTo(node.e) > 0){
+            //用else if 判断后相当于排除了第三种可能，既待插入元素值与当前节点值相等时什么也不做
+        } else if (e.compareTo(node.e) > 0) {
             node.right = add(node.right, e);
         }
 
@@ -71,29 +78,31 @@ public class BST2<E extends Comparable<E> > {
 
     /**
      * 看二分搜索树中是否包含某个元素e
+     *
      * @param e
      * @return
      */
-    public boolean contains(E e){
+    public boolean contains(E e) {
         return contains(root, e);
     }
 
     /**
      * 二分搜索树中是否包含某个元素e，递归算法
+     *
      * @param node
      * @param e
      * @return
      */
-    private boolean contains(Node node, E e){
-        if (node == null){
+    private boolean contains(Node node, E e) {
+        if (node == null) {
             return false;
         }
 
-        if (e.compareTo(node.e) == 0){
+        if (e.compareTo(node.e) == 0) {
             return true;
-        }else if (e.compareTo(node.e) < 0){
+        } else if (e.compareTo(node.e) < 0) {
             return contains(node.left, e);
-        }else {
+        } else {
             return contains(node.right, e);
         }
     }
@@ -101,12 +110,13 @@ public class BST2<E extends Comparable<E> > {
     /**
      * 二分搜索树的前序遍历
      */
-    public void preOder(){
+    public void preOder() {
         preOder(root);
     }
 
     /**
      * 前序遍历以node为根的二分搜索树, 递归算法
+     *
      * @param node
      */
     private void preOder(Node node) {
@@ -119,6 +129,234 @@ public class BST2<E extends Comparable<E> > {
         preOder(node.left);
         preOder(node.right);
     }
+
+    /**
+     * 二分搜索数树的非递归前序遍历
+     */
+    public void preOrderNR() {
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            Node curr = stack.pop();
+            System.out.println(curr.e);
+
+            if (curr.right != null){
+                stack.push(curr.right);
+            }
+
+             if (curr.left != null){
+                stack.push(curr.left);
+            }
+        }
+    }
+
+    /**
+     * 二分搜索树的中序遍历
+     */
+    public void inOder(){
+        inOder(root);
+    }
+
+    /**
+     * 中序遍历以node为根的二分搜索树, 递归算法
+     * 中序遍历得到的结果就是二分搜索树排序后的结果
+     * @param node
+     */
+    private void inOder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        inOder(node.left);
+        System.out.println(node.e);
+        inOder(node.right);
+    }
+
+    /**
+     * 二分搜索树的后续遍历
+     */
+    public void postOder(){
+        postOder(root);
+    }
+
+    /**
+     * 后序遍历以node为根的二分搜索树, 递归算法
+     * 后序遍历的应用 为二分搜索树释放内存
+     * @param node
+     */
+    private void postOder(Node node){
+        if (node == null){
+            return;
+        }
+
+        postOder(node.left);
+        postOder(node.right);
+        System.out.println(node.e);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateString(root, 0, res);
+        return res.toString();
+    }
+
+    /**
+     * 二分搜索树的层序遍历 (广度优先遍历)
+     */
+    public void levelOrder(){
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node curr = queue.remove();
+            System.out.println(curr.e);
+
+            if (curr.left != null){
+                queue.add(curr.left);
+            }
+            if (curr.right != null) {
+                queue.add(curr.right);
+            }
+        }
+    }
+
+    /**
+     * 寻找二分搜索树的最小元素
+     * @return
+     */
+    public E minimum(){
+        if (size == 0){
+            throw new IllegalArgumentException("BST is empty.");
+        }
+
+       return minimum(root).e;
+    }
+
+    /**
+     * 返回以node为根的二分搜索树的最小值所在的节点
+     * @param node
+     * @return
+     */
+    private Node minimum(Node node){
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    /**
+     * 寻找二分搜索树的最大元素
+     * @return
+     */
+    public E maximum(){
+        if (size == 0){
+            throw new IllegalArgumentException("BST is empty.");
+        }
+
+        return maximum(root).e;
+    }
+
+    /**
+     * 返回以node为根的二分搜索树的最大值所在的节点
+     * @param node
+     * @return
+     */
+    private Node maximum(Node node){
+        if (node.right == null) {
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+    /**
+     * 从二分搜索树中删除最小值所在的点，返回最小值
+     * @return
+     */
+    public E removeMin(){
+        E ret = minimum();
+        root =  removeMin(root);
+        return ret;
+    }
+
+    /**
+     * 删除以node为根的二分搜索树的最小节点
+     * 返回删除节点后新的二分搜索树的根
+     * @param node
+     * @return
+     */
+    private Node removeMin(Node node){
+
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+
+        node.left =  removeMin(node.left);
+        return node;
+    }
+
+
+    /**
+     * 从二分搜索树中删除最小值所在的点，返回最小值
+     * @return
+     */
+    public E removeMax(){
+        E ret = maximum();
+        root =  removeMin(root);
+        return ret;
+    }
+
+    /**
+     * 删除以node为根的二分搜索树的最小节点
+     * 返回删除节点后新的二分搜索树的根
+     * @param node
+     * @return
+     */
+    private Node removeMax(Node node){
+
+        if (node.right == null) {
+            Node rightNode = node.left;
+            node.left = null;
+            size--;
+            return rightNode;
+        }
+
+        node.right =  removeMax(node.right);
+        return node;
+    }
+
+    /**
+     * 生成以node为根节点，深度为depth的描述二叉树的字符串
+     *
+     * @param node
+     * @param depth
+     * @param res
+     */
+    private void generateString(Node node, int depth, StringBuilder res) {
+        if (node == null) {
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateString(node.left, depth + 1, res);
+        generateString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+
 
 }
 
--- src/com/minos/datastructure/binarySearchTree/Main.java
+++ src/com/minos/datastructure/binarySearchTree/Main.java
@@ -7,12 +7,17 @@ package com.minos.datastructure.binarySearchTree;
 public class Main {
     public static void main(String[] args) {
         BST2<Integer> bst2 = new BST2<>();
-        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums = {5, 3, 6, 8, 4, 2};
         for(int num : nums){
             bst2.add(num);
         }
 
-        bst2.preOder();
+//        bst2.preOder();
+//        System.out.println();
+//        //bst2.inOder();
+//        bst2.preOrderNR();
+
+        bst2.levelOrder();
 
     }
 }
