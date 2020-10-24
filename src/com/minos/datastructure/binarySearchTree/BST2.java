ce6c1581501a1427753ae38b4a866a4412b3f996
minos <tyforbyte@gmail.com>
2020-10-24 15:26:30 +0800

    v1.1

--- src/com/minos/datastructure/binarySearchTree/BST2.java
+++ src/com/minos/datastructure/binarySearchTree/BST2.java
@@ -334,6 +334,57 @@ public class BST2<E extends Comparable<E>> {
     }
 
     /**
+     * 从二分搜索树中删除元素为e的节点
+     * @param e
+     */
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    public Node remove(Node node, E e){
+        if (node == null) {
+            return null;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        }else if ( e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        }else { // e == node.e
+
+            //待删除节点左子树为空
+            if (node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+
+            //待删除节点右子树为空
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+
+            }
+
+            //待删除节点左右子树均不为空的情况
+            //找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+            //用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+        }
+    }
+
+
+    /**
      * 生成以node为根节点，深度为depth的描述二叉树的字符串
      *
      * @param node
